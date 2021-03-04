655e146f223385ca4b40e7f6b10f207c9a927e87
Charles Jayson Dadios <cjldadios.uplb@gmail.com>
2021-03-04 14:20:03 +0800

    adding exercises

--- mern-exercise-tracker/backend/server.js
+++ mern-exercise-tracker/backend/server.js
@@ -18,7 +18,7 @@ connection.once('open', () => {
 	console.log("MongoDB database connection established successfully");
 });
 
-const exercisesRouter = require('./routes/users');
+const exercisesRouter = require('./routes/exercises');
 const usersRouter = require('./routes/users');
 
 app.use('/exercises', exercisesRouter);

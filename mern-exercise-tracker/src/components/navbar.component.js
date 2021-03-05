9bba995486f66953534b6b86d6784dfd98495fd3
Charles Jayson Dadios <cjldadios.uplb@gmail.com>
2021-03-05 21:36:35 +0800

    edited navbar brand, changing classname to className

--- mern-exercise-tracker/src/components/navbar.component.js
+++ mern-exercise-tracker/src/components/navbar.component.js
@@ -6,7 +6,7 @@ export default class Navbar extends Component {
     render() {
         return (
             <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-                <Link to="/" classname="navbar-brand">ExerTracker</Link>
+                <Link to="/" className="navbar-brand">ExerTracker</Link>
                 <div className="collapse navbar-collapse">
                     <ul className="navbar-nav mr-auto">
                         <li className="navbar-item">

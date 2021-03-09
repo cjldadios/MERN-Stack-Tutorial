1b3b89b8f3d638b50d99f5779ca525a29dbbd4b6
Charles Jayson Dadios <cjldadios.uplb@gmail.com>
2021-03-09 11:09:43 +0800

    ended in creating form

--- mern-exercise-tracker/src/components/create-exercise.component.js
+++ mern-exercise-tracker/src/components/create-exercise.component.js
@@ -1,10 +1,80 @@
 import React, { Component } from 'react';
 
 export default class CreateExercises extends Component {
+    constructor(props) {
+        super(props)
+
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onChangeDescription = this.onChangeDescription.bind(this);
+        this.onChangeDuration = this.onChangeDuration.bind(this);
+        this.onChangeDate = this.onChangeDate.bind(this);
+        this.onChangeSubmit = this.onChangeSubmit.bind(this);
+
+        this.state = {
+            username: '',
+            decription: '',
+            duration: 0,
+            date: new Date(),
+            users: []
+        }
+    }
+
+    componentDidMount() {
+        this.setState({
+            users: ['test user'],
+            username: 'test user'
+        });
+    }
+    
+    onChangeUsername(e) {
+        this.setState({
+            username: e.target.value
+        });
+    }
+
+    onChangeDescription(e) {
+        this.setState({
+            description: e.target.value
+        });
+    }
+
+    onChangeDuration(e) {
+        this.setState({
+            duration: e.target.value
+        });
+    }
+    
+    onChangeDate(date) {
+        this.setState({
+            date: date
+        });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        const exercise = {
+            username: this.state.username,
+            description: this.state.duration,
+            date: this.state.date
+        }
+
+        console.log(exercise)
+
+        window.location = '/';
+    }
+
     render() {
         return (
             <div>
-                <p>You are on the Create Exercises component!</p>
+                <h3>Create New Exercise Log</h3>
+                <form onSubmit={this.onSubmit}>
+                    <label>Username: </label>
+                    <select ref="userInput"
+                        required
+                        
+                    ></select>
+                </form>
             </div>
         )
     }
